4878986d9776ad7292e235e39dddc65943d6e99a
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2023-07-26 21:02:37 -0300

    feat:adding config mounted app

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    //PODEMOS AQUI COLOCAR AS OPCOES DE CONFIGURAÇÃO PRO NOSSO VUE APP
+    //VUE E REATIVO
+     data(){
+         return{
+             product:'SocDSADASDASDdDASDSAsaks',
+             description:"GATAO  DSA"
+         }
+     }
+})
